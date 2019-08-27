c70fc581b2f99dac21f5bfbd4a0c4e05c3e192f4
Kirk Kaiser <kirk@zothcorp.com>
2019-08-27 17:56:31 -0400

    Update shop name too

--- core/db/default/spree/stores.rb
+++ core/db/default/spree/stores.rb
@@ -2,7 +2,7 @@
 unless Spree::Store.where(code: 'spree').exists?
   Spree::Store.new do |s|
     s.code              = 'spree'
-    s.name              = 'Spree Demo Site'
+    s.name              = 'storedog'
     s.url               = 'example.com'
     s.mail_from_address = 'spree@example.com'
     s.default_currency  = 'USD'
