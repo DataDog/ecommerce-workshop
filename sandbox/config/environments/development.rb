f92dc49b97442ca95088c3b23965af5b7c7b5009
Kirk Kaiser <kirk@zothcorp.com>
2019-09-05 14:44:23 -0400

    Begin adding trace log correlation

--- sandbox/config/environments/development.rb
+++ sandbox/config/environments/development.rb
@@ -5,6 +5,19 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  # Lograge config
+  config.lograge.enabled = true
+
+  # We are asking here to log in RAW (which are actually ruby hashes). The Ruby logging is going to take care of the JSON formatting.
+  config.lograge.formatter = Lograge::Formatters::Raw.new
+
+  # This is useful if you want to log query parameters
+  config.lograge.custom_options = lambda do |event|
+    { :ddsource => ["ruby"],
+      :params => event.payload[:params].reject { |k| %w(controller action).include? k }
+    }
+  config.colorize_logging = false
+  end
 
   # Do not eager load code on boot.
   config.eager_load = false
