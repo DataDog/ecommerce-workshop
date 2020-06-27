385f05eba13045554ba6134f75db77e0594499b5
KP Kaiser <kirk@zothcorp.com>
2020-06-27 13:53:04 -0400

    Add liveconding env for N+1 query

--- discounts-service/bootstrap.py
+++ discounts-service/bootstrap.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from models import Discount, db
+from models import Discount, DiscountType, Influencer, db
 
 import os
 DB_USERNAME = os.environ['POSTGRES_USER']
@@ -23,9 +23,28 @@ def initialize_database(app, db):
     with app.app_context():
         db.drop_all()
         db.create_all()
-        first_discount = Discount('Black Friday', 'BFRIDAY', 5.1)
-        second_discount = Discount('SWEET SUNDAY', 'OFF', 300.1)
-        third_discount = Discount('Monday Funday', 'PARTY', 5242.1)
+        first_discount_type = DiscountType('Save with Sherry', 
+                                           'price * .8',
+                                           Influencer('Sherry'))
+        second_discount_type = DiscountType('Sunday Savings', 
+                                           'price * .9',
+                                           None)
+        third_discount_type = DiscountType('Monday Funday',
+                                           'price * .95',
+                                           None)
+        first_discount = Discount('Black Friday', 
+                                  'BFRIDAY', 
+                                  5.1,
+                                  first_discount_type)
+
+        second_discount = Discount('SWEET SUNDAY', 
+                                   'OFF',
+                                   300.1,
+                                   second_discount_type)
+        third_discount = Discount('Monday Funday', 
+                                  'PARTY', 
+                                  542.1,
+                                  third_discount_type)
         db.session.add(first_discount)
         db.session.add(second_discount)
         db.session.add(third_discount)
