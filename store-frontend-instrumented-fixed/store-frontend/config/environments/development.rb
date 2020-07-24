a742d23af0502afc39e00dcfc10531a3567e72ea
KP Kaiser <kirk@zothcorp.com>
2020-07-24 09:35:10 -0400

    Add whitelist IP to supress last message

--- store-frontend-instrumented-fixed/store-frontend/config/environments/development.rb
+++ store-frontend-instrumented-fixed/store-frontend/config/environments/development.rb
@@ -12,6 +12,8 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  config.web_console.whitelisted_ips = ['10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16']
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
