07a3b45c9b29c52e91cea655e826b806292b3d65
l0k0ms <pierre.guceski@datadoghq.com>
2020-08-11 10:02:47 +0200

    wrapping call to catch error properly

--- discounts-service-fixed/discounts.py
+++ discounts-service-fixed/discounts.py
@@ -1,6 +1,7 @@
 import requests
 import random
 import time
+import traceback
 
 from random_word import RandomWords
 
@@ -24,19 +25,26 @@ def hello():
 @app.route('/discount', methods=['GET', 'POST'])
 def status():
     if flask_request.method == 'GET':
-        discounts = Discount.query.options(joinedload('*')).all()
-        app.logger.info(f"Discounts available: {len(discounts)}")
+        try:
+            discounts = Discount.query.options(joinedload('*')).all()
+            app.logger.info(f"Discounts available: {len(discounts)}")
+            influencer_count = 0
+            for discount in discounts:
+                if discount.discount_type.influencer:
+                    influencer_count += 1
+            app.logger.info(f"Total of {influencer_count} influencer specific discounts as of this request")
+            return jsonify([b.serialize() for b in discounts])
 
-        influencer_count = 0
-        for discount in discounts:
-            if discount.discount_type.influencer:
-                influencer_count += 1
-        app.logger.info(f"Total of {influencer_count} influencer specific discounts as of this request")
-        return jsonify([b.serialize() for b in discounts])
+        except:
+            app.logger.error(f"An error occured while getting discounts.")
+            err = jsonify({'error': 'Error getting discout'})
+            err.status_code = 500
+            return err
+
     elif flask_request.method == 'POST':
         # create a new discount with random name and value
         discounts_count = len(Discount.query.all())
-        new_discount = Discount('Discount ' + str(discounts_count + 1), 
+        new_discount = Discount('Discount ' + str(discounts_count + 1),
                                 r.get_random_word(),
                                 random.randint(10,500))
         app.logger.info(f"Adding discount {new_discount}")
