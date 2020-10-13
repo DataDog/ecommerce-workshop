f9fb51184d908ec3ac92eb76f673d4ff0336cec4
Aaron Kalin <akalin@martinisoftware.com>
2020-10-13 16:17:25 -0500

    Setup and configure HELM chart
    This is a super rough draft. Need some :eyes: on this implementation
    because it doesn't feel very elegant to configure the HELM chart this
    way and I need a better or easier solution. Not sure if that means
    having a data structure which gets applied? Maybe a commented YAML?

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -7,6 +7,20 @@ terraform {
 # TF_VAR_do_token environment variable
 variable "do_token" {}
 
+variable "dd_chart_version" {
+  default = "2.4.24"
+}
+
+variable "dd_api_key" {
+  type        = string
+  description = "The Datadog API Key to configure for the agent"
+}
+
+variable "dd_app_key" {
+  type        = string
+  description = "The Datadog Application Key required for the Cluster Agent"
+}
+
 variable "do_region" {
   default = "nyc1"
 }
@@ -39,4 +53,103 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(
     digitalocean_kubernetes_cluster.ecommerce.kube_config[0].cluster_ca_certificate
   )
+}
+
+provider "helm" {
+  kubernetes {
+    host  = digitalocean_kubernetes_cluster.ecommerce.endpoint
+    token = digitalocean_kubernetes_cluster.ecommerce.kube_config[0].token
+    cluster_ca_certificate = base64decode(
+      digitalocean_kubernetes_cluster.ecommerce.kube_config[0].cluster_ca_certificate
+    )
+  }
+}
+
+resource "helm_release" "datadog" {
+  repository       = "https://helm.datadoghq.com"
+  name             = "datadog"
+  chart            = "datadog"
+  version          = var.dd_chart_version
+  namespace        = "datadog-agent"
+  create_namespace = true
+
+  set_sensitive {
+    name  = "datadog.apiKey"
+    value = var.dd_api_key
+  }
+
+  set_sensitive {
+    name  = "datadog.appKey"
+    value = var.dd_app_key
+  }
+
+  set {
+    name  = "datadog.clusterName"
+    value = digitalocean_kubernetes_cluster.ecommerce.name
+  }
+
+  set {
+    name  = "datadog.processAgent.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "datadog.processAgent.processCollection"
+    value = "true"
+  }
+
+  set {
+    name  = "clusterAgent.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "clusterAgent.image.repository"
+    value = "datadog/cluster-agent"
+  }
+
+  set {
+    name  = "clusterAgent.image.tag"
+    value = "latest"
+  }
+
+  set {
+    name  = "clusterAgent.image.pullPolicy"
+    value = "Always"
+  }
+
+  set {
+    name  = "clusterAgent.metricsProvider.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "datadog.orchestratorExplorer.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "agents.image.repository"
+    value = "datadog/agent"
+  }
+
+  set {
+    name  = "agents.image.tag"
+    value = "latest"
+  }
+
+  set {
+    name  = "agents.image.pullPolicy"
+    value = "Always"
+  }
+
+  set {
+    name  = "datadog.logs.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "datadog.logs.containerCollectAll"
+    value = "true"
+  }
 }
