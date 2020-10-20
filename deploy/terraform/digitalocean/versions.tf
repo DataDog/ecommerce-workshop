7162a70a3937a89e3684d3efaa71133c3dd8f7b1
Aaron Kalin <akalin@martinisoftware.com>
2020-10-20 15:07:28 -0500

    Version pin these providers
    We pinned them in the previous module setup and forgot to do that here.

--- deploy/terraform/digitalocean/versions.tf
+++ deploy/terraform/digitalocean/versions.tf
@@ -4,6 +4,14 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = "~> 1.23.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 1.13.2"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0.0"
+    }
   }
   required_version = ">= 0.13"
 }
