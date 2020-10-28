be66b0f88a9fb1a246c73cf2b30954be63154570
Aaron Kalin <akalin@martinisoftware.com>
2020-10-28 16:58:11 -0500

    Move the kubectl config out to the deploy folder
    I think this makes it easier to run kubectl commands from the deploy
    folder for HELM charts and all that jazz.

--- deploy/terraform/digitalocean/output.tf
+++ deploy/terraform/digitalocean/output.tf
@@ -1,4 +1,5 @@
 resource "local_file" "kube_config_server_yaml" {
-  filename          = format("%s/%s", path.root, "kube_config_server.yaml")
+  filename          = format("%s/../../%s", path.root, "kube_config_server.yaml")
   sensitive_content = digitalocean_kubernetes_cluster.k8s_cluster.kube_config[0].raw_config
+  file_permission   = "0600"
 }
