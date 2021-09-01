35d3a716525b5d6e9340518820d62f6fd93dc21d
Colin Cole <colin.cole@datadoghq.com>
2021-09-01 13:59:19 -0400

    add rsyslog

--- discounts-service/my-wrapper-script.sh
+++ discounts-service/my-wrapper-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start the first process
-service ssh start
+service ssh start && service rsyslog start
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start my_first_process: $status"
