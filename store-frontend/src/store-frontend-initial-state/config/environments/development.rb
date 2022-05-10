831b1cc2b00988304af7d7d5327e66b6dc63e56c
Andrew Krug <andrewkrug@gmail.com>
2022-05-10 12:17:31 -0700

    comment out rails inotify to better support m1 chipset macs

--- store-frontend/src/store-frontend-initial-state/config/environments/development.rb
+++ store-frontend/src/store-frontend-initial-state/config/environments/development.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Quiet down logger level and disable fragment logging
   config.log_level = :info
